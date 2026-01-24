4b2c36a9298ca6bfec7e7280d445039a3496138e
L-Marcel <lmgh1312@gmail.com>
2026-01-24 19:25:32 -0300

    [docs] Melhorando documentação

--- validation_derive/src/lib.rs
+++ validation_derive/src/lib.rs
@@ -19,67 +19,70 @@ use crate::{
 use proc_macro_error::{emit_error, proc_macro_error};
 use syn::{DeriveInput, spanned::Spanned};
 
-/// **Struct options:** `#[validate(context = <?type>, modify = <?bool>, payload = <?bool>, asyncronous = <?bool>)]`
+/// **Struct options:** `#[validate(context = <?type>, modify = <?bool>, payload = <?bool>, asynchronous = <?bool>)]`
 /// * Configures global validation behavior, context injection, and serialization hooks.
 ///
 /// **Repository:** `https://github.com/L-Marcel/validy`
 ///
 /// **Special attributes:**: `#[special(<rule>, ...)]`
-/// * `nested`: Validates the fields of a nested struct.
-/// * `for_each(config?(from_item = <?type>, to_collection = <?type>, from_collection = <?type>), <rule>)`: Applies validation rules to every element in a collection.
-/// * `from_type(value = <?type>)`: Defines a custom wrapper field type.
+/// * `nested`(value = <type>, wrapper = <?type>): Validates the fields of a nested struct. Warning: cyclical references can cause compilation issues.
+/// * `for_each`(config?(from_item = <?type>, to_collection = <?type>, from_collection = <?type>), <rule>): Applies validation rules to every element in a collection. The `from_item` arg from the optional `config` rule defines the type of each collection item. The `to_collection` arg defines the final type of the collection, and the `from_collection` arg defines the initial type. It's like a `from_type` adapter for collections.
+/// * `from_type`(value = <?type>): Defines the type of the field in the wrapper. Must be defined before all other rules on a field.
 ///
 /// **Validation attributes:** `#[validate(<rule>, ...)]`
 ///
-/// *Existence & Options:*
-/// * `required(message = <?string>, code = <?string>)`: Changes the default message and code displayed when a field is missing.
-/// * `is_some(message = <?string>, code = <?string>)`: Validates that an `Option` is `Some`.
-/// * `is_none(message = <?string>, code = <?string>)`: Validates that an `Option` is `None`.
+/// *For `required` fields:*
+/// * `required`(message = <?string>, code = <?string>): Overrides the default message and code for a missing field. This rule requires the `payload` attribute to be enabled on the struct.
 ///
-/// *String & Patterns:*
-/// * `contains(slice = <string>, message = <?string>, code = <?string>)`: Validates that the string contains the specified substring.
-/// * `email(message = <?string>, code = <?string>)`: Validates that the string follows a standard email format.
-/// * `url(message = <?string>, code = <?string>)`: Validates that the string is a standard URL.
-/// * `ip(message = <?string>, code = <?string>)`: Validates that the string is a valid IP address (v4 or v6).
-/// * `ipv4(message = <?string>, code = <?string>)`: Validates that the string is a valid IPv4 address.
-/// * `ipv6(message = <?string>, code = <?string>)`: Validates that the string is a valid IPv6 address.
-/// * `pattern(pattern = <regex>, message = <?string>, code = <?string>)`: Validates that the string matches the provided Regex pattern.
-/// * `suffix(suffix = <string>, message = <?string>, code = <?string>)`: Validates that the string ends with the specified suffix.
-/// * `prefix(prefix = <string>, message = <?string>, code = <?string>)`: Validates that the string starts with the specified prefix.
+/// *For `string` fields:*
+/// * `contains`(slice = <string>, message = <?string>, code = <?string>): Validates that the string contains the specified substring.
+/// * `uuid`(message = <?string>, code = <?string>): Validates that the string is a valid UUID. This does not parse the string.
+/// * `email`(message = <?string>, code = <?string>): Validates that the string follows a standard email format.
+/// * `url`(message = <?string>, code = <?string>): Validates that the string is a standard URL. Finding good regex patterns for URLs is difficult and tedious, so I used the pattern `(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)` found [here](https://stackoverflow.com/a/3809435).
+/// * `ip`(message = <?string>, code = <?string>): Validates that the string is a valid IP address (v4 or v6).
+/// * `ipv4`(message = <?string>, code = <?string>): Validates that the string is a valid IPv4 address.
+/// * `ipv6`(message = <?string>, code = <?string>): Validates that the string is a valid IPv6 address.
+/// * `pattern`(pattern = <regex>, message = <?string>, code = <?string>): Validates that the string matches the provided Regex pattern.
+/// * `suffix`(suffix = <string>, message = <?string>, code = <?string>): Validates that the string ends with the specified suffix.
+/// * `prefix`(prefix = <string>, message = <?string>, code = <?string>): Validates that the string starts with the specified prefix.
+/// * `length`(range = <range>, message = <?string>, code = <?string>): Validates that the length of a string or collection is within the specified range.
 ///
-/// *Numbers & Collections:*
-/// * `range(range = <range>, message = <?string>, code = <?string>)`: Validates that the number falls within the specified numeric range.
-/// * `length(range = <range>, message = <?string>, code = <?string>)`: Validates that the length (string or collection) is within limits.
-/// * `any(items = <array>, message = <?string>, code = <?string>)`: Validates that the value is present in the allowed list (allowlist).
-/// * `none(items = <array>, message = <?string>, code = <?string>)`: Validates that the value is NOT present in the forbidden list (blocklist).
+/// *For `collection` or `single` fields:*
+/// * `length`(range = <range>, message = <?string>, code = <?string>): Validates that the length of a string or collection is within the specified range.
+/// * `allowlist`(mode = <"SINGLE" \| "COLLECTION">, items = <array>, message = <?string>, code = <?string>): Validates that the value or collection items are present in the allowlist.
+/// * `blocklist`(mode = <"SINGLE" \| "COLLECTION">, items = <array>, message = <?string>, code = <?string>): Validates that the value or collection items are NOT present in the blocklist.
 ///
-/// *Date & Time:*
-/// * `time(format = <string>, message = <?string>, code = <?string>)`: Validates that the string matches the specified time/date format.
-/// * `naive_time(format = <string>, message = <?string>, code = <?string>)`: Validates that the string matches the specified naive time format.
-/// * `naive_date(format = <string>, message = <?string>, code = <?string>)`: Validates that the string matches the specified naive date format.
-/// * `after_now(accept_equals = <?bool>, message = <?string>, code = <?string>)`: Validates that the date/time is strictly after the current time.
-/// * `before_now(accept_equals = <?bool>, message = <?string>, code = <?string>)`: Validates that the date/time is strictly before the current time.
-/// * `now(ms_tolerance = <?int>, message = <?string>, code = <?string>)`: Validates that the date/time matches the current time within a tolerance (default: 500ms).
-/// * `after_today(accept_equals = <?bool>, message = <?string>, code = <?string>)`: Validates that the date is strictly after the current day.
-/// * `before_today(accept_equals = <?bool>, message = <?string>, code = <?string>)`: Validates that the date is strictly before the current day.
-/// * `today(message = <?string>, code = <?string>)`: Validates that the date matches the current day.
+/// *For `numbers` fields:*
+/// * `range`(range = <range>, message = <?string>, code = <?string>): Validates that the number falls within the specified numeric range.
 ///
-/// *Custom:*
-/// * `inline(closure = <closure>, params = <?array>, message = <?string>, code = <?string>)`: Validates using a simple inline closure returning a boolean.
-/// * `custom(function = <function>, params = <?array>)`: Validates using a custom function.
-/// * `custom_with_context(function = <function>, params = <?array>)`: Validates using a custom function with access to the context.
-/// * `async_custom(function = <function>, params = <?array>)`: Validates using a custom async function.
-/// * `async_custom_with_context(function = <function>, params = <?array>)`: Validates using a custom async function with access to the context.
+/// *For `date` or `time` fields:*
+/// * `time`(format = <string>, message = <?string>, code = <?string>): Validates that the string matches the specified `DateTime<FixedOffset>` format. This does not parse the string.
+/// * `naive_time`(format = <string>, message = <?string>, code = <?string>): Validates that the string matches the specified `NaiveDateTime` format. This does not parse the string.
+/// * `naive_date`(format = <string>, message = <?string>, code = <?string>): Validates that the string matches the specified `NaiveDate` format. This does not parse the string.
+/// * `after_now`(accept_equals = <?bool>, message = <?string>, code = <?string>): Validates that the `DateTime<FixedOffset>` is strictly after the current time.
+/// * `before_now`(accept_equals = <?bool>, message = <?string>, code = <?string>): Validates that the `DateTime<FixedOffset>` is strictly before the current time.
+/// * `now`(ms_tolerance = <?int>, message = <?string>, code = <?string>): Validates that the `DateTime<FixedOffset>` matches the current time within a tolerance (default: 500ms).
+/// * `after_today`(accept_equals = <?bool>, message = <?string>, code = <?string>): Validates that the `NaiveDate` is strictly after the current day.
+/// * `before_today`(accept_equals = <?bool>, message = <?string>, code = <?string>): Validates that the `NaiveDate` is strictly before the current day.
+/// * `today`(message = <?string>, code = <?string>): Validates that the `NaiveDate` matches the current day.
+///
+/// *Custom rules:*
+/// * `inline`(closure = <closure>, params = <?array>, message = <?string>, code = <?string>): Validates using a simple inline closure returning a boolean.
+/// * `custom`(function = <function>, params = <?array>): Validates using a custom function.
+/// * `custom_with_context`(function = <function>, params = <?array>): Validates using a custom function with access to the context.
+/// * `async_custom`(function = <function>, params = <?array>): Validates using a custom async function.
+/// * `async_custom_with_context`(function = <function>, params = <?array>): Validates using a custom async function with access to the context.
 ///
 /// **Modification attributes:** `#[modify(<modifier>, ...)]`
 ///
-/// *String & Cases:*
+/// *For `string` fields:*
+/// * `parse_uuid`: Validates that a string is a valid UUID and parses it.
 /// * `trim`: Removes whitespace from both ends of the string.
 /// * `trim_start`: Removes whitespace from the start of the string.
 /// * `trim_end`: Removes whitespace from the end of the string.
 /// * `uppercase`: Converts all characters in the string to uppercase.
 /// * `lowercase`: Converts all characters in the string to lowercase.
-/// * `capitalize`: Capitalizes the first character of the string.
+/// * `capitalize`: Capitalizes the first character of each word in the string.
 /// * `camel_case`: Converts the string to CamelCase (PascalCase).
 /// * `lower_camel_case`: Converts the string to lowerCamelCase.
 /// * `snake_case`: Converts the string to snake_case.
@@ -88,17 +91,17 @@ use syn::{DeriveInput, spanned::Spanned};
 /// * `shouty_kebab_case`: Converts the string to SHOUTY-KEBAB-CASE.
 /// * `train_case`: Converts the string to Train-Case.
 ///
-/// *Date & Time Parsing:*
-/// * `parse_time(format = <string>, message = <?string>, code = <?string>)`: Validates and parses that the string matches the specified time/date format.
-/// * `parse_naive_time(format = <string>, message = <?string>, code = <?string>)`: Validates and parses that the string matches the specified naive time format.
-/// * `parse_naive_date(format = <string>, message = <?string>, code = <?string>)`: Validates and parses that the string matches the specified naive date format.
+/// *For `date` or `time` fields:*
+/// * `parse_time`(format = <string>, message = <?string>, code = <?string>): Validates and parses a string into a `DateTime<FixedOffset>` matching the specified format.
+/// * `parse_naive_time`(format = <string>, message = <?string>, code = <?string>): Validates and parses a string into a `NaiveDateTime` matching the specified format.
+/// * `parse_naive_date`(format = <string>, message = <?string>, code = <?string>): Validates and parses a string into a `NaiveDate` matching the specified format.
 ///
-/// *Custom:*
-/// * `inline(closure = <closure>, params = <?array>)`: Modifies the value using an inline closure.
-/// * `custom(function = <function>, params = <?array>)`: Modifies the value in-place using a custom function.
-/// * `custom_with_context(function = <function>, params = <?array>)`: Modifies the value in-place using a custom function with context access.
-/// * `async_custom(function = <function>, params = <?array>)`: Modifies the value in-place using a custom async function.
-/// * `async_custom_with_context(function = <function>, params = <?array>)`: Modifies the value in-place using a custom async function with context access.
+/// *Custom rules:*
+/// * `inline`(closure = <closure>, params = <?array>): Modifies the value using an inline closure.
+/// * `custom`(function = <function>, params = <?array>): Modifies the value using a custom function.
+/// * `custom_with_context`(function = <function>, params = <?array>): Modifies the value using a custom function with context access.
+/// * `async_custom`(function = <function>, params = <?array>): Modifies the value using a custom async function.
+/// * `async_custom_with_context`(function = <function>, params = <?array>): Modifies the value using a custom async function with context access.
 #[proc_macro_error]
 #[proc_macro_derive(Validate, attributes(validate, modify, special))]
 pub fn validation_macro(input: Input) -> Output {
