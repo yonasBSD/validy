21697c9ae9580c1b01dd1c27ffe7e0fb2d41f185
L-Marcel <lmgh1312@gmail.com>
2026-01-24 18:16:20 -0300

    [feat] Adicionando macros do uuid

---
+++ validation_derive/src/primitives/uuids/uuid.rs
@@ -0,0 +1,86 @@
+use std::cell::RefCell;
+
+use proc_macro_error::emit_error;
+use proc_macro2::{Span, TokenStream};
+use quote::quote;
+use syn::{Error, LitStr, Result, parse::ParseStream};
+
+use crate::{
+	ImportsSet,
+	fields::FieldAttributes,
+	imports::Import,
+	primitives::commons::{ArgParser, parse_attrs, remove_parens},
+};
+
+pub struct UuidArgs {
+	pub code: LitStr,
+	pub message: LitStr,
+}
+
+impl Default for UuidArgs {
+	fn default() -> Self {
+		UuidArgs {
+			code: LitStr::new("uuid", Span::call_site()),
+			message: LitStr::new("invalid uuid format", Span::call_site()),
+		}
+	}
+}
+
+impl ArgParser for UuidArgs {
+	const POSITIONAL_KEYS: &'static [&'static str] = &["message", "code"];
+
+	fn apply_value(&mut self, name: &str, input: ParseStream) -> Result<()> {
+		match name {
+			"code" => self.code = input.parse()?,
+			"message" => self.message = input.parse()?,
+			_ => return Err(Error::new(input.span(), "unknown arg")),
+		}
+
+		Ok(())
+	}
+}
+
+pub fn create_uuid(input: ParseStream, field: &mut FieldAttributes, imports: &RefCell<ImportsSet>) -> TokenStream {
+	imports
+		.borrow_mut()
+		.add(Import::ValidationFunction("uuids::validate_uuid as validate_uuid_fn"));
+
+	let field_name = field.get_name();
+	let reference = field.get_reference();
+	let content = remove_parens(input);
+
+	let UuidArgs { code, message } = match content {
+		Ok(content) => parse_attrs(&content)
+			.inspect_err(|erro| emit_error!(erro.span(), "{}", erro))
+			.unwrap_or_default(),
+		Err(_) => UuidArgs::default(),
+	};
+
+	if field.is_ref() {
+		field.set_is_ref(true);
+		#[rustfmt::skip]
+		let result = quote! {
+			if can_continue(&errors, failure_mode, #field_name) && let Err(e) = validate_uuid_fn(#reference, #field_name, #code, #message) {
+        append_error(&mut errors, e, failure_mode, #field_name);
+        if should_fail_fast(&errors, failure_mode, #field_name) {
+     			return Err(errors);
+     	  }
+		  }
+		};
+
+		result
+	} else {
+		field.set_is_ref(false);
+		#[rustfmt::skip]
+		let result = quote! {
+			if can_continue(&errors, failure_mode, #field_name) && let Err(e) = validate_uuid_fn(&#reference, #field_name, #code, #message) {
+        append_error(&mut errors, e, failure_mode, #field_name);
+        if should_fail_fast(&errors, failure_mode, #field_name) {
+     			return Err(errors);
+     	  }
+		  }
+		};
+
+		result
+	}
+}
