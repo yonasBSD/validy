09f3f13c453dbf1ef11b92c81dd460739ef04073
L-Marcel <lmgh1312@gmail.com>
2026-01-29 18:01:57 -0300

    [test] Adicionando mais testes

---
+++ tests/parsings/last_fail_per_field/ipv6.rs
@@ -0,0 +1,34 @@
+use std::net::Ipv6Addr;
+
+use validy::core::{Validate, ValidateAndParse};
+
+use validy::assert_errors;
+
+#[derive(Debug, Validate, PartialEq)]
+#[validate(payload, failure_mode = LastFailPerField)]
+#[wrapper_derive(Debug, Clone)]
+struct Test {
+	#[special(from_type(String))]
+	#[parse(ipv6("custom message", "custom_code"))]
+	pub a: Ipv6Addr,
+	#[special(from_type(String))]
+	#[parse(ipv6("custom message", "custom_code"))]
+	pub b: Option<Ipv6Addr>,
+}
+
+#[test]
+fn should_parse_ipv6s() {
+	let cases = [""];
+
+	let mut wrapper = TestWrapper::default();
+	for case in cases.iter() {
+		wrapper.a = Some(case.to_string());
+		wrapper.b = Some(case.to_string());
+		let result = Test::validate_and_parse(wrapper.clone());
+
+		assert_errors!(result, wrapper, {
+			"a" => [("custom_code", "custom message")],
+			"b" => [("custom_code", "custom message")],
+		});
+	}
+}
